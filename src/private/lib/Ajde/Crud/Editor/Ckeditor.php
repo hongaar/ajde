fdac886a517dee399de5b6cda3abd750433c73a3
hongaar <hongaar@gmail.com>
2013-08-10 15:36:02 +0200

    ckeditor help

--- src/private/lib/Ajde/Crud/Editor/Ckeditor.php
+++ src/private/lib/Ajde/Crud/Editor/Ckeditor.php
@@ -5,6 +5,12 @@ class Ajde_Crud_Editor_Ckeditor extends Ajde_Crud_Editor
 	function getResources(&$view) {
 		/* @var $view Ajde_Template_Parser_Phtml_Helper */
 		
+		// Installed?
+		if (!file_exists(PUBLIC_DIR . 'js/_core/crud/editor/ckeditor/lib/ckeditor.js')) {
+			// @todo
+			throw new Ajde_Exception('CKEditor not installed, see <a href="https://github.com/hongaar/ajde/tree/master/src/public/js/_core/crud/editor/ckeditor">README</a>');
+		}
+		
 		// Library files
 		$view->requireJsPublic('_core/crud/editor/ckeditor/lib/ckeditor.js');
 		$view->requireJsPublic('_core/crud/editor/ckeditor/lib/adapters/jquery.js');
---
+++ src/public/js/_core/crud/editor/ckeditor/README.md
@@ -0,0 +1,8 @@
+Install
+-------------------------
+
+#### CKEditor
+
+ - Please download the CKEditor package from http://ckeditor.com/download
+ - Copy the contents of the downloaded archive in the 'lib' directory.
+ - Copy the onchange plugin to the plugins directory
