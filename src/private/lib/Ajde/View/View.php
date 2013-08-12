e44a2d980ac2a92fe338a4529df63ea3b5284ac2
hongaar <hongaar@gmail.com>
2013-08-12 21:21:57 +0200

    this broke some things

--- src/private/lib/Ajde/View/View.php
+++ src/private/lib/Ajde/View/View.php
@@ -8,14 +8,14 @@ class Ajde_View extends Ajde_Template
 	 * @return Ajde_View
 	 */
 	public static function fromController(Ajde_Controller $controller) {
-		$base = MODULE_DIR. $controller->getModule() . DIRECTORY_SEPARATOR; 
+		$base = MODULE_DIR. $controller->getModule() . '/'; 
 		$action = $controller->getRoute()->getController() ?
-			$controller->getRoute()->getController() . DIRECTORY_SEPARATOR . $controller->getAction() :
+			$controller->getRoute()->getController() . '/' . $controller->getAction() :
 			$controller->getAction();			
 		$format = $controller->hasFormat() ? $controller->getFormat() : 'html';			
 		return new self($base, $action, $format);
 	}
-	
+
 	/**
 	 *
 	 * @param Ajde_Core_Route $route
@@ -26,9 +26,9 @@ class Ajde_View extends Ajde_Template
 		if (!$route instanceof Ajde_Core_Route) {
 			$route = new Ajde_Core_Route($route);
 		}
-		$base = MODULE_DIR. $route->getModule() . DIRECTORY_SEPARATOR;
+		$base = MODULE_DIR. $route->getModule() . '/';
 		$action = $route->getController() ?
-			$route->getController() . DIRECTORY_SEPARATOR . $route->getAction() :
+			$route->getController() . '/' . $route->getAction() :
 			$route->getAction();
 		$format = $route->getFormat();
 		return new self($base, $action, $format);
