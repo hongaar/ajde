b9c77b8df0909c49278946cb5cb883e6ecd58049
hongaar <hongaar@gmail.com>
2013-08-29 14:42:21 +0200

    system check and update

---
+++ src/private/application/modules/admin/AdminSystemController.php
@@ -0,0 +1,83 @@
+<?php 
+
+class AdminSystemController extends AdminController
+{	
+	public function check()
+	{
+		Ajde::app()->getDocument()->setTitle("System check");
+		
+		$checks = array();
+		
+		$checks[] = array(
+			'msg'	=> 'Directories writable?',
+			'fn'	=> 'writable'
+		);
+		$checks[] = array(
+			'msg'	=> 'Files deleted?',
+			'fn'	=> 'deleted'
+		);
+		
+		$ret = array();
+		
+		foreach($checks as $check) {
+			$ret = call_user_func(array($this, 'check' . ucfirst($check['fn'])));
+			if (empty($ret)) {
+				$ret = array(array('msg' => 'OK', 'status' => 'success'));
+			}
+			foreach($ret as $re) {
+				$results[] = array(
+					'check'		=> $check['msg'],
+					'msg'		=> $re['msg'],
+					'status'	=> $re['status']
+				);
+			}
+		}
+		
+		$this->getView()->assign('results', $results);
+		return $this->render();
+	}
+	
+	public function update()
+	{
+		Ajde::app()->getDocument()->setTitle("Ajde updater");
+		
+		$this->getView()->assign('curversion', AJDE_VERSION);
+		$this->getView()->assign('availableversion', $results);
+		
+		return $this->render();
+	}
+	
+	private function checkDeleted()
+	{
+		$files = array(
+				'../phpinfo.php', '../loadtest.php', '../test'
+		);
+		$ret = array();
+		foreach($files as $file) {
+			if (file_exists($file)) {
+				$ret[] = array(
+						'msg'		=> 'File ' . $file . ' should be deleted in production environment',
+						'status'	=> 'warning'
+				);
+			}
+		}
+		return $ret;
+	}
+	
+	private function checkWritable()
+	{
+		$dirs = array(
+			TMP_DIR, LOG_DIR, CACHE_DIR
+		);
+		$ret = array();
+		foreach($dirs as $dir) {
+			if (!is_writable($dir)) {
+				$ret[] = array(
+					'msg'		=> 'Directory ' . $dir . ' is not writable',
+					'status'	=> 'important'
+				);
+			}
+		}
+		return $ret;
+	}
+}
